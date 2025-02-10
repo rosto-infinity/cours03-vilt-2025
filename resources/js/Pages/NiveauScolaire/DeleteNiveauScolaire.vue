66573e5a8374079c0e61acf47a739b0b02ff155b
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-10 09:52:36 +0100

    delete

--- resources/js/Pages/NiveauScolaire/DeleteNiveauScolaire.vue
+++ resources/js/Pages/NiveauScolaire/DeleteNiveauScolaire.vue
@@ -41,7 +41,7 @@ const deleteNiveauScolaire = () => {
 <template>
   <div>
     <!-- Bouton de suppression -->
-    <SecondaryButton @click="openDeleteModal" class="bg-red-500 hover:bg-red-700 text-white p-1 rounded-md">
+    <SecondaryButton @click="openDeleteModal" class="bg-[red] hover:bg-red-700 text-white p-[2px] rounded-md">
       <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M1 7h22M8 7V4a1 1 0 011-1h6a1 1 0 011 1v3" />
@@ -57,10 +57,10 @@ const deleteNiveauScolaire = () => {
           <strong>{{ niveauScolaire.nom }}</strong> ?
         </p>
         <div class="flex justify-end space-x-3">
-          <SecondaryButton @click="closeDeleteModal" class="bg-gray-500 hover:bg-gray-700 text-white">
+          <SecondaryButton @click="closeDeleteModal" class="bg-[#25d425] hover:bg-[#258825] text-white">
             Annuler
           </SecondaryButton>
-          <PrimaryButton @click="deleteNiveauScolaire" class="bg-red-500 hover:bg-red-700 text-white">
+          <PrimaryButton @click="deleteNiveauScolaire" class="bg-[red] hover:bg-red-700 text-white">
             Supprimer
           </PrimaryButton>
         </div>
