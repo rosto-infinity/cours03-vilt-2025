47d7dafd7261c2bd0ce9cc39eb5d37070efe9091
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-14 19:09:55 +0100

    welcome

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        Vous êtes connectez
+                        Vous êtes connectez !
                     </div>
                 </div>
             </div>
